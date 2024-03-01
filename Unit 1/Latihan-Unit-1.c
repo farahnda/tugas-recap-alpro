969954aa5fdf467285965a019ddf4a0f5997c32b
farahnda <161808068+farahnda@users.noreply.github.com>
2024-03-01 21:24:05 +0700

    Tugas Unit 1

---
+++ Unit 1/Latihan-Unit-1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+    int jarak1;
+    int waktu1;
+    int kecepatan1;
+    int waktu2;
+    int jarak2;
+    int kecepatan2;
+
+    scanf("%d", &jarak1);
+    scanf("%d", &waktu1);
+    scanf("%d", &kecepatan1);
+    scanf("%d", &waktu2);
+    scanf("%d", &jarak2);
+    scanf("%d", &kecepatan2);
+
+    float V = (float) jarak1 / waktu1;
+    float S = (float) kecepatan1 * waktu2;
+    float T = (float) jarak2 / kecepatan2;
+
+    printf("%f\n", V);
+    printf("%f\n", S);
+    printf("%f\n", T);
+
+    return 0;
+
+}
